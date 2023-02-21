68be92ae97196731a4e88d7e065c140219377b8b
mskang21 <minsungk0212@gmail.com>
2023-02-21 17:21:12 -0500

    Copied the previous files into solution 4 folder

---
+++ solution-hw4/js/hw4.js
@@ -0,0 +1,68 @@
+console.log("Javascript Loaded");
+
+let allGlazings = [
+    {
+        glazing:'Keep original',
+        priceAdapt:'0.00',
+    },
+    {
+        glazing:'Sugar milk',
+        priceAdapt:'0.00',
+    },
+    {
+        glazing:'Vanilla milk',
+        priceAdapt:'0.50',
+    },
+    {
+        glazing:'Double chocolate',
+        priceAdapt:'1.50',
+    },
+];
+let allPacksizes = [
+    {
+        packsize:'1',
+        priceAdapt:'1',
+    },
+    {
+        packsize:'3',
+        priceAdapt:'3',
+    },
+    {
+        packsize:'6',
+        priceAdapt:'5',
+    },
+    {
+        packsize:'12',
+        priceAdapt:'10',
+    },
+]
+
+let basePrice = 2.49;
+let glazing = 0;
+let packsize = 1;
+
+function displayPrice(GlazingToDisplay, PacksizeToDisplay) {
+    let PriceElement = document.querySelector('#price');
+    console.log(GlazingToDisplay);
+    PriceElement.innerText = "$" + (basePrice + GlazingToDisplay) * PacksizeToDisplay;
+}
+function GlazingChange(event){
+    console.log(event);
+    console.log('You selected' + this.value);
+    let GlazingIndex = parseInt(this.value);
+    console.log(allGlazings[GlazingIndex - 1]);
+    glazing = parseFloat(allGlazings[GlazingIndex - 1].priceAdapt);
+    displayPrice(glazing, packsize);
+}
+function PacksizeChange(event){
+    console.log('You selected' + this.value);
+    let PacksizeIndex = parseInt(this.value);
+    packsize = parseInt(allPacksizes[PacksizeIndex - 1].priceAdapt);
+    displayPrice(glazing, packsize);
+}
+
+const selectElement = document.querySelector('#glazingOptions');
+let selectElement2 = document.querySelector('#packsizeOptions');
+
+selectElement.addEventListener('change', GlazingChange);
+selectElement2.addEventListener('change', PacksizeChange);
