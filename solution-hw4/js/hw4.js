e0c519a4f5991b79c7a1b88b2413adb6680a00b6
mskang21 <minsungk0212@gmail.com>
2023-02-22 18:39:17 -0500

    updated the script tags

--- solution-hw4/js/hw4.js
+++ solution-hw4/js/hw4.js
@@ -36,7 +36,6 @@ let allPacksizes = [
         priceAdapt:'10',
     },
 ]
-
 let basePrice = 2.49;
 let glazing = 0;
 let packsize = 1;
@@ -44,7 +43,8 @@ let packsize = 1;
 function displayPrice(GlazingToDisplay, PacksizeToDisplay) {
     let PriceElement = document.querySelector('#price');
     console.log(GlazingToDisplay);
-    PriceElement.innerText = "$" + (basePrice + GlazingToDisplay) * PacksizeToDisplay;
+    let totalPrice = (basePrice + GlazingToDisplay) * PacksizeToDisplay;
+    PriceElement.innerText = "$" + totalPrice.toFixed(2)
 }
 function GlazingChange(event){
     console.log(event);
@@ -57,7 +57,7 @@ function GlazingChange(event){
 function PacksizeChange(event){
     console.log('You selected' + this.value);
     let PacksizeIndex = parseInt(this.value);
-    packsize = parseInt(allPacksizes[PacksizeIndex - 1].priceAdapt);
+    packsize = parseFloat(allPacksizes[PacksizeIndex - 1].priceAdapt);
     displayPrice(glazing, packsize);
 }
 
@@ -69,12 +69,25 @@ selectElement2.addEventListener('change', PacksizeChange);
 
 const queryString = window.location.search;
 console.log(queryString);
-const params = new URLSearchParams(querySring);
+const params = new URLSearchParams(queryString);
 console.log(params);
-const rolltype = params.get('roll')
-console.log(rolltype)
+const rollType = params.get('roll')
+console.log(rollType)
 
-const headerElement = document.querySelector('#');
-headerElement.innerText = 'Here is the ' + rollType + '!'
+const headerElement = document.querySelector('#pageTitle');
+headerElement.innerText = rollType
 const rollImage = document.querySelector('#enlargedRoll');
 rollImage.src = './images/' + rollType + '.jpg';
+
+class Roll {
+    constructor(rollType, glazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing =  glazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+}
+
+
+
+
