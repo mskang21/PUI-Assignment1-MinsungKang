6886528211f952436fbf194f5b208a9f00f7593b
mskang21 <minsungk0212@gmail.com>
2023-02-22 13:44:25 -0500

    updating js code and html code URL params

--- solution-hw4/js/hw4.js
+++ solution-hw4/js/hw4.js
@@ -66,3 +66,15 @@ let selectElement2 = document.querySelector('#packsizeOptions');
 
 selectElement.addEventListener('change', GlazingChange);
 selectElement2.addEventListener('change', PacksizeChange);
+
+const queryString = window.location.search;
+console.log(queryString);
+const params = new URLSearchParams(querySring);
+console.log(params);
+const rolltype = params.get('roll')
+console.log(rolltype)
+
+const headerElement = document.querySelector('#');
+headerElement.innerText = 'Here is the ' + rollType + '!'
+const rollImage = document.querySelector('#enlargedRoll');
+rollImage.src = './images/' + rollType + '.jpg';
